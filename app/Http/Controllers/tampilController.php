d774d3b625e5e355679d87950fe79fc8ae0fdbd7
masdazaii <anggitprayoga@mail.ugm.ac.id>
2018-11-06 09:52:02 +0700

    proyek aplikasi

---
+++ app/Http/Controllers/tampilController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class tampilController extends Controller
+{
+    public function tampil(){
+    	return view('tampil');
+    }
+}
---
+++ resources/views/tampil.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+
+@section('div')
+	hello ini adalah blank
+@endsection
